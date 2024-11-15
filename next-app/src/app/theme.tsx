3c10ddaa638ffbdf47d9c38b07ee1c0ed180c85c
asm9677 <asm9677@naver.com>
2024-11-16 00:00:58 +0700

    feat: header

--- next-app/src/app/theme.tsx
+++ next-app/src/app/theme.tsx
@@ -1,18 +1,56 @@
 /* theme.ts */
 import { extendTheme } from "@chakra-ui/react";
 import { GlobalStyleProps } from "@chakra-ui/theme-tools";
-import { mode } from "@chakra-ui/theme-tools";
-
 const colors = {
-  
+  primary: "#007bff",
+  dark: "#0d1117",
+  gray: {
+    100: "#E0E0E0",
+    300: "#A0A0A0",
+    500: "#797979",
+    700: "#3B3D59",
+    800: "#212120",
+    900: "#0d1117",
+  },
+  blue: {
+    300: "#2d9fb9",
+    700: "#234753",
+  },
+  cyan: {
+    300: "#02ebff",
+  },
+  teal: {
+    200: "#2cb997",
+    700: "#2c6356",
+    800: "#1d322d",
+  },
+
+  red: {
+    400: "#CC3B3B",
+  },
+  // gold: "#FEC824",
+  gold: "#E1B52E",
+  green: {
+    100: "#c7f284",
+    200: "#94e449",
+    300: "#89CF96",
+    500: "#4AB75C",
+    600: "#41A052",
+    700: "#508365",
+  },
 };
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
+import { mode } from "@chakra-ui/theme-tools";
 
 const styles = {
-  
+  global: (props: GlobalStyleProps) => ({
+    body: {
+      bg: mode("dark", "#0d1117")(props),
+    },
+  }),
 };
 
 export const theme = extendTheme({
---
+++ next-app/src/components/organisms/Header.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import NetworkButton from "../molecules/NetworkButton";
+import WalletButton from "../molecules/WalletButton";
+import { TbWallet } from "react-icons/tb";
+import { useAccount } from "@/context/AccountProvider";
+import { PiLetterCirclePBold } from "react-icons/pi";
+import { MdSwapCalls } from "react-icons/md";
+import { FaHandHoldingUsd } from "react-icons/fa";
+import NavButton from "../molecules/NavButton";
+import Image from "next/image";
+
+const navLinks = [
+  {
+    name: <Text textColor={"white"}>Points Market</Text>,
+    path: "/?token=all",
+    icon: <PiLetterCirclePBold size={24} />,
+  },
+  {
+    name: <Text textColor={"#d2d2d2"}>Pre-Market</Text>,
+    path: "/pre",
+    icon: <MdSwapCalls size={24} />,
+  },
+  {
+    name: <Text textColor={"#d2d2d2"}>OTC Market</Text>,
+    path: "/otc",
+    icon: <FaHandHoldingUsd size={24} />,
+  },
+];
+
+export const Header = () => {
+  const { signer, connectWallet, connectProvider } = useAccount();
+
+  useEffect(() => {
+    connectProvider();
+    if (localStorage.getItem("loggedIn")) {
+      connectWallet();
+    }
+  }, []);
+
+  return (
+    <Box
+      display="flex"
+      mx="auto"
+      w="1280px"
+      h="64px"
+      justifyContent="space-between"
+      color="white"
+      letterSpacing="tight"
+    >
+      <Flex alignItems={"center"}>
+        <Image src="/images/Flexir.png" alt="Flexir" width={52} height={30} />
+        {navLinks.map((v, i) => (
+          <NavButton
+            key={i}
+            title={v.name}
+            link={v.path}
+            icon={v.icon}
+            isDisabled={i === 0 ? false : true}
+          />
+        ))}
+      </Flex>
+      <Box
+        display="flex"
+        w="450px"
+        alignItems="center"
+        pr="4"
+        gap="4"
+        justifyContent="flex-end"
+      >
+        <NetworkButton />
+        {signer ? (
+          <WalletButton />
+        ) : (
+          <Button
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            rounded="4px"
+            h="40px"
+            px="12px"
+            py="6px"
+            w="150px"
+            fontSize="14px"
+            fontWeight="600"
+            textColor="#15161B"
+            bgColor={"green.200"}
+            _hover={{ backgroundColor: "green.200" }}
+            onClick={connectWallet}
+          >
+            <Box pr="1">
+              <TbWallet size={24} />
+            </Box>
+            Connect Wallet
+          </Button>
+        )}
+      </Box>
+    </Box>
+  );
+};
