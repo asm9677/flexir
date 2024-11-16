56a1bd9c08186e8693c7c67275c8e9293010cae6
inevitablechange <yongho7783@gmail.com>
2024-11-16 12:20:29 +0700

    오퍼 카드, 탭 추가

---
+++ next-app/src/app/myoffer/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import {
+  Flex,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import { useEffect, useMemo, useState } from "react";
+import { useAccount } from "@/context/AccountProvider";
+import { Contract } from "ethers";
+import { contracts } from "@/contracts/addresses";
+import { getOfferEvents, getOrderEvents } from "@/features/events";
+import MyOffersTab from "@/components/myoffer/MyOffersTab";
+
+export default function Page() {
+  const { signer } = useAccount();
+
+  const [mainTabIndex, setMainTabIndex] = useState<number>(0); // 0: My Offers, 1: My Orders
+  const [subTabIndex, setSubTabIndex] = useState<number>(0); // Sub-tabs for each main tab
+  const [pointMarketContract, setPointMarketContract] =
+    useState<Contract | null>(null);
+  const [offers, setOffers] = useState<Offer[]>([]);
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  const filteredOffers = useMemo(() => {
+    return offers.filter((offer) => {
+      if (subTabIndex === 0) {
+        return offer.offerStatus === 1;
+      } else if (subTabIndex === 1) {
+        return offer.offerStatus === 2;
+      }
+      return true;
+    });
+  }, [offers, subTabIndex]);
+
+  const fetchEvents = async () => {
+    if (!pointMarketContract || !signer) return;
+
+    const [offers, orders] = await Promise.all([
+      getOfferEvents(pointMarketContract, signer.address),
+      getOrderEvents(pointMarketContract, signer.address),
+    ]);
+
+    setOffers(offers);
+    setOrders(orders);
+  };
+
+  useEffect(() => {
+    if (!signer) return;
+
+    const pointMarketCtr = new Contract(
+      contracts.flexir.address,
+      contracts.flexir.abi,
+      signer
+    );
+
+    setPointMarketContract(pointMarketCtr);
+  }, [signer]);
+
+  useEffect(() => {
+    if (!pointMarketContract || !signer) return;
+
+    fetchEvents();
+  }, [pointMarketContract, signer]);
+
+  return (
+    <Flex h="100vh" color="white" flexDir="column" alignItems="center">
+      <Tabs
+        index={mainTabIndex}
+        colorScheme="white"
+        onChange={(index) => {
+          setMainTabIndex(index);
+          setSubTabIndex(0);
+        }}
+        variant="enclosed"
+        w="100%"
+        maxW="1280px"
+        px={10}
+        mt={2}
+      >
+        <TabList borderColor={"green.700"}>
+          <Tab>My Offers</Tab>
+          <Tab>My Positions</Tab>
+        </TabList>
+
+        <TabPanels>
+          {/* My Offers Tab */}
+          <TabPanel>
+            <MyOffersTab
+              subTabIndex={subTabIndex}
+              setSubTabIndex={setSubTabIndex}
+              offers={offers}
+              setOffers={setOffers}
+              filteredOffers={filteredOffers}
+              pointMarketContract={pointMarketContract}
+            />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Flex>
+  );
+}
--- next-app/src/contracts/addresses.ts
+++ next-app/src/contracts/addresses.ts
@@ -3,15 +3,15 @@ import ERC20ABI from "./abis/ERC20ABI.json";
 
 export const contracts = {
   flexir: {
-    address: "0x000000000000000000000000000000000000dead",
+    address: "0xa482D491B6320B5f27B6bB5816Acaa7A8Bab7D51",
     abi: flexirABI,
   },
   token: {
-    address: "0x000000000000000000000000000000000000dead",
+    address: "0x84572b7757d167684193E34c0bB02381CA85bea7",
     abi: ERC20ABI,
   },
   usdt: {
-    address: "0x000000000000000000000000000000000000dead",
+    address: "0x44567F968430c01CEa34C8dE3A9ee4BC55d27cd5",
     abi: ERC20ABI,
   },
 };
