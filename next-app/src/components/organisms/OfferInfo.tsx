2b7f0ae72dc9027b505f0bff37015697d221cc1f
gyuhaaa <vkvkvk2892@naver.com>
2024-11-17 02:22:27 +0700

    feat: apply blockExplorerUrl

--- next-app/src/components/organisms/OfferInfo.tsx
+++ next-app/src/components/organisms/OfferInfo.tsx
@@ -48,12 +48,13 @@ const OfferInfo: FC<OfferInfoProps> = ({
   setSettleStatus,
   collateral,
 }) => {
-  const { signer } = useAccount();
+  const { signer, curChain } = useAccount();
   const { flexirContract } = useContract();
   const [originOfferTx, setOriginOfferTx] = useState<string>("");
 
   useEffect(() => {
     fetchOriginOffer();
+    console.log(curChain);
   }, [offer]);
 
   const fetchOriginOffer = async () => {
@@ -126,9 +127,7 @@ const OfferInfo: FC<OfferInfoProps> = ({
               {offer.offeredBy.slice(-3)}
             </Text>
             <Link
-              href={`${networks.find((v) => {
-                v.chainId = chainId;
-              })}https://sepolia.etherscan.io/address/${offer.offeredBy}`}
+              href={`${curChain?.blockExplorerUrl}/address/${offer.offeredBy}`}
               target="_blank"
             >
               <GoLinkExternal color="white" />
@@ -143,7 +142,7 @@ const OfferInfo: FC<OfferInfoProps> = ({
               {originOfferTx?.slice(0, 5)}...{originOfferTx?.slice(-3)}
             </Text>
             <Link
-              href={`https://sepolia.etherscan.io/tx/${originOfferTx}`}
+              href={`${curChain?.blockExplorerUrl}/tx/${originOfferTx}`}
               target="_blank"
             >
               <GoLinkExternal color="white" />
